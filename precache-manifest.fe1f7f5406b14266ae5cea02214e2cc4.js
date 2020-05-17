2c6dbc6f6b0b7579a8141937a2ee0654852ed6a7
Gabriel Linassi <gabrielm.linassi@hotmail.com>
2020-05-17 06:26:47 -0300

    Updates

---
+++ precache-manifest.fe1f7f5406b14266ae5cea02214e2cc4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "835ae74dc7e813a1a278ae949c78a4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d247683670a9353f90c",
+    "url": "/static/css/main.ee365975.chunk.css"
+  },
+  {
+    "revision": "30d4ca40c33c691be7d9",
+    "url": "/static/js/2.6120dc63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6120dc63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d247683670a9353f90c",
+    "url": "/static/js/main.699e316b.chunk.js"
+  },
+  {
+    "revision": "a87b682811c3a7a39760",
+    "url": "/static/js/runtime-main.90a906cb.js"
+  }
+]);
